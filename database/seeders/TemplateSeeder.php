29af63ec7fe7f3f1bd27eaba5c268eaf25390b85
tiaswidiatmika <tiaswidiatmika@gmail.com>
2021-09-24 15:31:48 +0800

    add new template example

--- database/seeders/TemplateSeeder.php
+++ database/seeders/TemplateSeeder.php
@@ -16,13 +16,25 @@ class TemplateSeeder extends Seeder
     {
     // protected $fillable = ['template_name', 'case', 'summary', 'chronology', 'measure', 'conclusion'];
 
-        Template::create([
-            'template_name' => 'cerita kanak-kanak',
-            'case' => 'pada hari [hari] ku turut ayah ke [tempat]',
-            'summary' => 'senang sekali [kegiatan] di [tempat]',
-            'chronology' => 'pada hari [hari] ku turut ayah ke [tempat]',
-            'measure' => 'pada hari [hari] ku turut ayah ke [tempat]',
-            'conclusion' => 'pada hari [hari] ku turut ayah ke [tempat]',
-        ]);
+        $example = [
+            'nihil' => [
+                'template_name' => 'Laporan Nihil',
+                'case' => '<i>NIHIL</i>',
+                'summary' => '<i>NIHIL</i>',
+                'chronology' =>'<i>NIHIL</i>',
+                'measure' =>'<i>NIHIL</i>',
+                'conclusion' =>'<i>NIHIL</i>',
+            ],
+            'charter kru turun' => [
+                'template_name' => 'Pemeriksaan Pesawat Charter (Kru Turun)',
+                'case' => 'Pemeriksaan [bagian kedatangan / keberangkatan] terhadap [jumlah awak] awak alat angkut pesawat charter [nama pesawat] ([kode penerbangan]) rute penerbangan [rute penerbangan].',
+                'summary' => 'Bahwa, pada hari, tanggal tersebut di atas, telah dilakukan pemeriksaan [bagian kedatangan / keberangkatan] terhadap [jumlah awak] orang kru pesawat charter [nama pesawat] ([kode penerbangan]), rute [rute penerbangan] pada pukul [waktu mendarat] WITA di Terminal Selatan Bandar Udara Internasional Ngurah Rai Denpasar, dengan rincian terlampir.',
+                'chronology' => 'Pesawat tiba pada pukul [waktu mendarat] WITA di Terminal Selatan Bandara I Gusti Ngurah Rai. Pemeriksaan imigrasi juga dilakukan di terminal selatan oleh Petugas Imigrasi',
+                'measure' => '1. Melaporkan kejadian tersebut kepada pimpinan.<br>2. Melakukan pemeriksaan terhadap [jumlah awak] orang kru',
+                'conclusion' => 'Bahwa telah dilakukan pemeriksaan keimigrasian terhadap [jumlah awak] orang kru pesawat charter [nama pesawat] ([kode penerbangan])',
+            ]
+        ];
+
+        foreach ($example as $key => $value) Template::create($value);
     }
 }
